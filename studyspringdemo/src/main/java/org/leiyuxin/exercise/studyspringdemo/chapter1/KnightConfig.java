46b616375c829f6cc5c49a77a31d64e4f8a44e57
leiyuxin <617705033@qq.com>
2018-04-10 02:03:59 +0800

    以注解的方式获取bean
    Signed-off-by: leiyuxin <617705033@qq.com>

---
+++ studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter1/KnightConfig.java
@@ -0,0 +1,17 @@
+package org.leiyuxin.exercise.studyspringdemo.chapter1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KnightConfig {
+
+	@Bean
+	public Knight knight() {
+		return new BraveKnight(quest());
+	}
+	@Bean
+	public Quest quest() {
+		return new SlayDragonQuest(System.out);
+	}
+}
--- studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter1/KnightMain.java
+++ studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter1/KnightMain.java
@@ -1,13 +1,21 @@
 package org.leiyuxin.exercise.studyspringdemo.chapter1;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class KnightMain {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("knights.xml");
-		//Knight knight = (Knight)context.getBean("knight"); 与下面的等价
+		//Knight knight = (Knight)context.getBean("knight"); 与下面以class的方式等价
 		Knight knight = context.getBean(Knight.class);
 		knight.embarkOnQuest();
 		context.close();
+
+
+		AnnotationConfigApplicationContext AnnotationContext = new AnnotationConfigApplicationContext(KnightConfig.class);
+	  // String[] arrary = AnnotationContext.getBeanDefinitionNames("knight");
+	    knight = AnnotationContext.getBean(BraveKnight.class);
+	    knight.embarkOnQuest();
+	    AnnotationContext.close();
 	}
 }
