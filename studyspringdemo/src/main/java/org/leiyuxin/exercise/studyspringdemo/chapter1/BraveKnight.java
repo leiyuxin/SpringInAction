e0661403278ad344cc64069fe647fd318a3f4783
leiyuxin <617705033@qq.com>
2018-04-11 04:03:37 +0800

    bean 的生命周期： 1，Spring 对bean进行实例化， 2，Spring 将值和bean的引用注入到bean 对应的属性中； 3，如果bean 实现了BeanNameAware 接口，Spring 将bean的ID传递给setBeanName（）方法； 4，如果bean 实现了BeanFactoryAware 接口，Spring 将调用setBeanFactory()方法，将BeanFactory容器实例传入 5.如果Bean实现了ApplicationContextAware 接口，Spring 将调用setApplicationContext（）方法，将bean 所在的应用上下文的引用传入进来， 6，如果bean 实现了BeanPostProcessor 接口，Spring 将调用他们的postProcessBeforeInitialization()方法， 7.如果bean 实现了InitializingBean接口，Spring 将调用他们的afterPropertiesSet()方法，类似地，如果bean使用Init-method 的声明了初始化方法，该方法也会被调用； 8.如果bean 实现了BeanPostProcessor 接口，Spring将调用他们的post-processAfterInitialization()方法； 9，此时，bean以及准备就绪。可以被应用程序使用了。他们将一直驻留在应用上下文中，直到，该应用上下文被销毁。 10，如果bean 实现了， DisposableBean 接口，Spring 将调用它的destroy()接口，方法同样，如果bean使用destroy-method声明了销毁了方法，该方法也会被调用。 Signed-off-by: leiyuxin <617705033@qq.com>

--- studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter1/BraveKnight.java
+++ studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter1/BraveKnight.java
@@ -1,6 +1,9 @@
 package org.leiyuxin.exercise.studyspringdemo.chapter1;
 
-public class BraveKnight implements Knight {
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.wiring.BeanConfigurerSupport;
+
+public class BraveKnight extends BeanConfigurerSupport implements Knight,InitializingBean {
 	private Quest quest;
 	public BraveKnight(Quest quest) {//构造器注入 没有自行创建探险任务。
 		this.quest = quest;
@@ -8,4 +11,10 @@ public class BraveKnight implements Knight {
 	public void embarkOnQuest() {
 		quest.embark();
 	}
+/*	@Override
+	public void afterPropertiesSet() {
+
+		System.out.println("InitializingBean interface  afterProertiesSet");
+
+	}*/
 }
--- studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter1/Knight.java
+++ studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter1/Knight.java
@@ -1,7 +1,16 @@
 package org.leiyuxin.exercise.studyspringdemo.chapter1;
 
+import org.springframework.beans.factory.BeanFactory;
+
+import com.mysql.cj.jdbc.Driver;
+
+/*import org.springframework.jdbc.core.JdbcTemplate;*/
+
 public interface Knight {
 	public default void embarkOnQuest() {
 		System.out.println("default knight");
+		/*new JdbcTemplate().queryForObject(sql, rowMapper, args);*/
+
+
 	}
 }
