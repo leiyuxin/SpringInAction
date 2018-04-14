43e67adfa14d7c7cd67410b1468f9832abb2523f
leiyuxin <617705033@qq.com>
2018-04-15 01:23:52 +0800

    显示的通过配置文件生成Bean
    Signed-off-by: leiyuxin <617705033@qq.com>

--- studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter2/MainTest.java
+++ studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter2/MainTest.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainTest {
 	public static void main(String[] args) {
+		// 通过自动配置，隐式生成Bean
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CDPlayerConfig.class);
 		// CompactDisc cd = context.getBean(CompactDisc.class);
 		// CompactDisc cd = (CompactDisc) context.getBean("sgtPeppers");//Spring为bean
@@ -13,6 +14,14 @@ public class MainTest {
 		cd.play();
 
 		System.out.println(context.getBean("lyx"));
+		System.out.println(context.getBeanDefinitionCount());
+		context.close();
+
+		// 通过java 配置 显示生成bean;
+		context = new AnnotationConfigApplicationContext(CDPlayerConfigBaseOnExplicit.class);
+		cd = (CompactDisc) context.getBean("lyx");
+		cd.play();
+		System.out.println(cd);
 		System.out.println(context.getBeanDefinitionCount());
 		context.close();
 
