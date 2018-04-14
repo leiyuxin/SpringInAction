bee6b423724014f9628173c2826ba7bef9bfaaa0
leiyuxin <617705033@qq.com>
2018-04-14 20:51:58 +0800

    在扫描基础包的java配置文件中 加入不同的配置包的方式
    Signed-off-by: leiyuxin <617705033@qq.com>

--- studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter2/CDPlayerConfig.java
+++ studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter2/CDPlayerConfig.java
@@ -1,11 +1,26 @@
 package org.leiyuxin.exercise.studyspringdemo.chapter2;
 
+import org.leiyuxin.exercise.studyspringdemo.App;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
+/**
+ *
+ * @author lyx
+ *
+ * 字符串的命中的包名在重构代码的时候如果包名改变了那么指定的基础包名也将改变，有可能出现错误
+ *
+ *组件ComponentScan  支持将指定为包中所包含的类或接口，而这些类或接口所在的包将作为组件扫描的基础包
+ */
+
+//@ComponentScan("org.leiyuxin.exercise.studyspringdemo.chapter2") //使用字符串的方式作为基础包的值。简写形式
+//@ComponentScan(basePackages="org.leiyuxin.exercise.studyspringdemo.chapter2")//更加清楚的形式  字为字符串
+//@ComponentScan(basePackages= {"org.leiyuxin.exercise.studyspringdemo.chapter2"})//如果有多个基础包要扫描可以使用这种方式{package1,package2...}  字为字符串
 //@CompontScan在Spring 启用组件扫描，如果没有其他配置这个注解默认会扫描与配置类相同的包,以及子包z,查找带有@Component的类并且为其创建bean
+
+//@ComponentScan(basePackageClasses= {CDPlayerConfig.class}) //支持多个
+@ComponentScan(basePackageClasses= {App.class}) //包的层级更加高
+@Configuration
 public class CDPlayerConfig {
 
 }
--- studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter2/SgtPeppers.java
+++ studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter2/SgtPeppers.java
@@ -1,20 +1,19 @@
 package org.leiyuxin.exercise.studyspringdemo.chapter2;
 
-import javax.inject.Named;
+//import javax.inject.Named;
 
 import org.springframework.stereotype.Component;
 
-//@Component(value="specialComponentName")       //这个注解表明该类会作为组件类，并告知Spring要为这个类创建bean
-@Named("specialComponentName")  //Named 与Component 注解是等价的 Named 使用的java依赖注入规范
+@Component(value = "specialComponentName") // 这个注解表明该类会作为组件类，并告知Spring要为这个类创建bean
+// @Named("specialComponentName") //Named 与Component 注解是等价的 Named 使用的java依赖注入规范
+// 推荐使用Component
 public class SgtPeppers implements CompactDisc {
 	private String title = "Sgt. Pepper's Lonely Hears Club Band";
 	private String artist = "The Beatles";
+
 	@Override
 	public void play() {
 		System.out.println("Test 成功");
 	}
-
-
-
 
 }
