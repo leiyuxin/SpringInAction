b9b56e19c55c46e557ed4647464d6c0e2b40fd63
leiyuxin <617705033@qq.com>
2018-04-14 19:56:02 +0800

    使用xml配置文件的方式装配bean
    Signed-off-by: leiyuxin <617705033@qq.com>

--- studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter2/MainTest.java
+++ studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter2/MainTest.java
@@ -1,11 +1,17 @@
 package org.leiyuxin.exercise.studyspringdemo.chapter2;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainTest {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context  = new AnnotationConfigApplicationContext(CDPlayerConfig.class);
 		CompactDisc cd = context.getBean(CompactDisc.class);
 		cd.play();
+		context.close();
+		ClassPathXmlApplicationContext contextXml = new ClassPathXmlApplicationContext("autoConfig.xml");
+		cd = contextXml.getBean(CompactDisc.class);
+		cd.play();
+		context.close();
 	}
 }
