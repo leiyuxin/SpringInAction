bebdfcb9372a55579376e66ebe015a75b666b130
leiyuxin <617705033@qq.com>
2018-04-14 21:29:49 +0800

    1.大多数情况注解Component 与Named 注解，Inject 注解与Autowired 注解是等价的 当使用Autowired 注解设置false 时要有非空检查否则会报空指针错误 当有多个bean 满足的时候会报错
    Signed-off-by: leiyuxin <617705033@qq.com>

--- studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter2/CDPlayer.java
+++ studyspringdemo/src/main/java/org/leiyuxin/exercise/studyspringdemo/chapter2/CDPlayer.java
@@ -1,5 +1,7 @@
 package org.leiyuxin.exercise.studyspringdemo.chapter2;
 
+import javax.inject.Inject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,13 +19,14 @@ public class CDPlayer implements MediaPlayer {
 		System.out.println("CDPlayer 工作了");
 
 	}
-	@Autowired
+	@Autowired(required=false)  //如果required 设置为false 意味着所需要的bean 可以不存在不会报错，如果有多个bean满足则会报错，设置false 小心空指针
 	public void insertDisc(CompactDisc cd) {
 		this.cd = cd;
 	}
 
-	@Autowired
+	@Inject //大多数情况下Inject 与Autowired 等价的但是，它没有 require的属性
 	public void insertDisc() {
 		System.out.println("在检查到Autowired注解的时候调用此方法");
 	}
+
 }
